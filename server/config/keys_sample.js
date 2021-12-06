8b9418ddf61d7aa585754c52c1e80fdff2a9bf0e
peijen <peijen@ualberta.ca>
2021-12-06 15:35:15 -0800

    Modify config setting for Dev and Prod environment
    - remove keys_sample.js
    - add keys.js for auto determining environment
    - add prod.js for production environment variables
    - update gitignore for dev environment

--- server/config/keys_sample.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    googleClientID: "Your Google Client ID",
-    googleClientSecret: "Your Google Client Secret",
-    mongoURI: "Your mongoDB URI",
-    cookieKey: "Your cookie encryption Key"
-}
